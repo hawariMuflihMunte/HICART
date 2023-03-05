706ea5158ee6da13dcf4cf78780af1cb4b489cda
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-03-05 19:50:51 +0700

    Migrate to ReactJS

---
+++ src/views/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const REACT_VERSION = React.version;
+
+function App() {
+	return (
+		<div className='App'>React v{REACT_VERSION}</div>
+	);
+}
+
+export default App;
