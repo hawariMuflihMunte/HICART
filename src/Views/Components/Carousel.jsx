f2bb00dfd149128e004fbad32718d588aaf6ead5
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-03-06 21:48:46 +0700

    Init Carousel.jsx, Carousel.scss

---
+++ src/Views/Components/Carousel.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Carousel.scss';
+
+// Assignees:
+// @iqbaalma
+
+function Carousel() {
+	return (
+		<section className='carousel'>
+
+		</section>
+	);
+}
+
+export default Carousel;
