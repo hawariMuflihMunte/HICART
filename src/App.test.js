0cf9ea6783f48d00d25b42edebe25e5f1e66a8fe
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-03-06 11:07:37 +0700

    Update + Fix import reference, import CSS Resetter

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import {render, screen} from '@testing-library/react';
-import App from './views/App';
+import App from './Views/App';
 
 test('renders learn react link', () => {
 	render(<App />);
