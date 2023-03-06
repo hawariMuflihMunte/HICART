ceed8cdd05ae69c00ee9fa176d180826ac83f349
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-03-06 10:18:21 +0700

    Finishing Navbar

--- src/views/components/Navbar.jsx
+++ src/views/components/Navbar.jsx
@@ -9,7 +9,29 @@ function Navbar() {
 			<a href='#' className='nav-brand'>
 				<img src='https://raw.githubusercontent.com/hawariMuflihMunte/HICART/docs/HICART%20ASSETS/LOGO/logo%20HICART-03.png' alt='HICART' />
 			</a>
-			<input className='nav-search' type='text' placeholder='Mau cari apa ?' />
+			<section className='nav-category'>
+				<a href='#'>
+					<i className='material-symbols-rounded'>widgets</i>
+				</a>
+			</section>
+			<section className='nav-search'>
+				<label htmlFor='navSearch'><i className='material-symbols-rounded'>search</i></label>
+				<input type='text' id='navSearch' placeholder='Mau cari apa ?' />
+			</section>
+			<section className='nav-interactive'>
+				<a href='#'>
+					<i className='material-symbols-rounded'>shopping_bag</i>
+				</a>
+				<a href='#'>
+					<i className='material-symbols-rounded'>notifications</i>
+				</a>
+				<a href='#'>
+					<i className='material-symbols-rounded'>mail</i>
+				</a>
+				<a href='#'>
+					<i className='material-symbols-rounded'>account_circle</i>
+				</a>
+			</section>
 		</nav>
 	);
 }
