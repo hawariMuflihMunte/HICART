9e46a45c5388a72a552d214fc8428db691840d87
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-03-08 21:50:01 +0700

    Delete unused, undefined Section

--- src/Views/App.jsx
+++ src/Views/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Navbar from './Components/Navbar.jsx';
 import Carousel from './Components/Carousel';
-import Section from './Components/Section.jsx';
 
 import './App.scss';
 
@@ -11,7 +10,6 @@ function App() {
 		<main className='container'>
 			<Navbar />
 			<Carousel />
-			<Section SectionClassName='red' />
 		</main>
 	);
 }
