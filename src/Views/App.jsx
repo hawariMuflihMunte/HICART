b3aee53870b8314a18f11c3a858e8c13bfc25d0d
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-03-11 23:40:11 +0700

    Init Section

--- src/Views/App.jsx
+++ src/Views/App.jsx
@@ -4,12 +4,14 @@ import Navbar from './Components/Navbar.jsx';
 import Carousel from './Components/Carousel';
 
 import './App.scss';
+import Section from './Components/Section.jsx';
 
 function App() {
 	return (
 		<main className='container'>
 			<Navbar />
 			<Carousel />
+			<Section title1='Kategori' />
 		</main>
 	);
 }
--- src/Views/Components/Section.jsx
+++ src/Views/Components/Section.jsx
@@ -1,9 +1,46 @@
+/* eslint-disable react/prop-types */
+// Assignees: @hawariMuflihMunte
 import React from 'react';
 
-function Section() {
-	return (
-		<section>
+import './Section.scss';
 
+function Section({
+	title1,
+	title2 = '',
+}) {
+	if (title2 !== '') {
+		return (
+			<section className='item-category'>
+				<div className='item-container'>
+					<h3 className='item-title'>{title1}</h3>
+					<div className='item-display'>
+						<img src='https://i.pinimg.com/564x/fe/db/be/fedbbe73caf0d294c846aee8a8e6540a.jpg' alt='Gadget & Smartphone' loading='lazy' />
+						<img src='https://whatsnewindonesia.com/sites/default/files/styles/1280x800/public/2022-10/Furniture-stores-bali-760x500.jpg?itok=CGIf7-dO' alt='Furnitures' loading='lazy'/>
+						<img src='https://images.tokopedia.net/img/cache/500-square/VqbcmM/2021/4/21/914b0e5b-a111-4614-90c1-d3ae063d5ae0.png' alt='Style & Fashion' loading='lazy' />
+					</div>
+				</div>
+				<div className='item-container'>
+					<h3 className='item-title'>{title2}</h3>
+					<div className='item-display'>
+						<img src='https://i.pinimg.com/564x/fe/db/be/fedbbe73caf0d294c846aee8a8e6540a.jpg' alt='Gadget & Smartphone' loading='lazy' />
+						<img src='https://whatsnewindonesia.com/sites/default/files/styles/1280x800/public/2022-10/Furniture-stores-bali-760x500.jpg?itok=CGIf7-dO' alt='Furnitures' loading='lazy'/>
+						<img src='https://images.tokopedia.net/img/cache/500-square/VqbcmM/2021/4/21/914b0e5b-a111-4614-90c1-d3ae063d5ae0.png' alt='Style & Fashion' loading='lazy' />
+					</div>
+				</div>
+			</section>
+		);
+	}
+
+	return (
+		<section className='item-category'>
+			<div className='item-container'>
+				<h3>{title1}</h3>
+				<div className='item-display'>
+					<img src='https://i.pinimg.com/564x/fe/db/be/fedbbe73caf0d294c846aee8a8e6540a.jpg' alt='Gadget & Smartphone' loading='lazy' />
+					<img src='https://whatsnewindonesia.com/sites/default/files/styles/1280x800/public/2022-10/Furniture-stores-bali-760x500.jpg?itok=CGIf7-dO' alt='Furnitures' loading='lazy'/>
+					<img src='https://images.tokopedia.net/img/cache/500-square/VqbcmM/2021/4/21/914b0e5b-a111-4614-90c1-d3ae063d5ae0.png' alt='Style & Fashion' loading='lazy' />
+				</div>
+			</div>
 		</section>
 	);
 }
