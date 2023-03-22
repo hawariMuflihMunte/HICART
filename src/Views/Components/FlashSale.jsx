ac2d5af2446aaa8237151b70c66b56adeb25d093
Iqbal Maulana <suraatiqbaal@gmail.com>
2023-03-23 02:30:22 +0700

    Problem snapscroll and countdowntime && add build

--- src/Views/Components/FlashSale.jsx
+++ src/Views/Components/FlashSale.jsx
@@ -2,20 +2,102 @@ import React from 'react';
 
 import './FlashSale.scss';
 
+import ImageFlashSale1 from '../../Assets/Images/Section/FlashSale.jpg';
+
 function FlashSale() {
 	return (
 		<section className='flash-sale'>
-			<div className='flash-sale-content'>
-				<p className='promo-kilat'>Promo Kilat</p>
-			</div>
-			<div className='content-card'>
-				{/* card-1 */}
-				<div className='card-flash-sale'>
-					<img className='' src='' alt=''></img>
-					<p>Rp.20.000</p>
-					<p>Rp.200.000</p>
-					<p>Progress Bar</p>
-					<p>Tersedia</p>
+
+			<div className='flash-sale-inner'>
+				<div className='flash-time'>
+					<p className='promo-kilat'>Promo Kilat</p>
+					<div className='time'>
+						<div className='hh'>00</div>
+						<div>:</div>
+						<div className='mm'>05</div>
+						<div>:</div>
+						<div className='ss'>22</div>
+					</div>
+
+				</div>
+				<div className='flash-sale-content'>
+					{/* card-1 */}
+					<div className='content-card'>
+						<div className='card-flash-sale-1'>
+							<img className='image-flash-sale' src={ImageFlashSale1} alt='Image flash sale' />
+							<dix className='price'>Rp.100.000<span className='percent'>50%</span></dix>
+							<div className='discount'>Rp.200.000</div>
+							<div className='progress-bar progress-bar-50'>
+							</div>
+							<p className='status'>Tersedia</p>
+						</div>
+					</div>
+					{/* card-2 */}
+					<div className='content-card'>
+						<div className='card-flash-sale-2'>
+							<img className='image-flash-sale' src={ImageFlashSale1} alt='Image flash sale' />
+							<dix className='price'>Rp.131.679<span className='percent'>67%</span></dix>
+							<div className='discount'>Rp.399.000</div>
+							<div className='progress-bar progress-bar-77'>
+							</div>
+							<p className='status'>Tersedia</p>
+						</div>
+					</div>
+					{/* card-3 */}
+					<div className='content-card'>
+						<div className='card-flash-sale-3'>
+							<img className='image-flash-sale' src={ImageFlashSale1} alt='Image flash sale' />
+							<dix className='price'>Rp.146.250<span className='percent'>25%</span></dix>
+							<div className='discount'>Rp.195.000</div>
+							<div className='progress-bar progress-bar-10'>
+							</div>
+							<p className='status'>Tersedia</p>
+						</div>
+					</div>
+					{/* card-4 */}
+					<div className='content-card'>
+						<div className='card-flash-sale-4'>
+							<img className='image-flash-sale' src={ImageFlashSale1} alt='Image flash sale' />
+							<dix className='price'>Rp.2.760<span className='percent'>77%</span></dix>
+							<div className='discount'>Rp.12.000</div>
+							<div className='progress-bar progress-bar-96'>
+							</div>
+							<p className='status'>Tersedia</p>
+						</div>
+					</div>
+					{/* card-5 */}
+					<div className='content-card'>
+						<div className='card-flash-sale-5'>
+							<img className='image-flash-sale' src={ImageFlashSale1} alt='Image flash sale' />
+							<dix className='price'>Rp.36.000<span className='percent'>40%</span></dix>
+							<div className='discount'>Rp.60.000</div>
+							<div className='progress-bar progress-bar-20'>
+							</div>
+							<p className='status'>Tersedia</p>
+						</div>
+					</div>
+					{/* card-6 */}
+					<div className='content-card'>
+						<div className='card-flash-sale-6'>
+							<img className='image-flash-sale' src={ImageFlashSale1} alt='Image flash sale' />
+							<dix className='price'>Rp.1.000<span className='percent'>99%</span></dix>
+							<div className='discount'>Rp.10.999.999</div>
+							<div className='progress-bar progress-bar-100'>
+							</div>
+							<p className='status'>Habis</p>
+						</div>
+					</div>
+					{/* card-7 */}
+					<div className='content-card'>
+						<div className='card-flash-sale-7'>
+							<img className='image-flash-sale' src={ImageFlashSale1} alt='Image flash sale' />
+							<dix className='price'>Rp.1.200<span className='percent'>80%</span></dix>
+							<div className='discount'>Rp.6.000</div>
+							<div className='progress-bar progress-bar-89'>
+							</div>
+							<p className='status'>Tersedia</p>
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>
